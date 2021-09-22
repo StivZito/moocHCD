21e29acac6db7d177cb46bf03da9bb6e912b2178
Stiven A. Prado <zito351@gmail.com>
2021-09-22 12:31:50 -0500

    ;;

--- puntosExtras/config/global.php
+++ puntosExtras/config/global.php
@@ -1,7 +1,7 @@
 <?php 
-	define("DB_HOST","67.205.158.181");//ip server
+	define("DB_HOST","localhost");//ip server
 	define("DB_NAME","zardnet_moochcd");//database
-	define("DB_USERNAME","zardnet_moorem");//user
+	define("DB_USERNAME","zardnet_moochcd");//user
 	define("DB_PASSWORD", "Stiven!2021@");//password
 	define("DB_ENCODE","utf8");//encode
 	define("PROJ_NAME","pj_notasmooc");//project name
