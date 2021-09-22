4f123ab03c5087bbbc76996a4ddc3076d8004a40
Stiven A. Prado <zito351@gmail.com>
2021-09-22 12:12:58 -0500

    Update global.php

--- encuestas/config/global.php
+++ encuestas/config/global.php
@@ -1,8 +1,8 @@
 <?php 
-	define("DB_HOST","localhost");//ip server
-	define("DB_NAME","moochcd");//database
-	define("DB_USERNAME","root");//user
-	define("DB_PASSWORD", "");//password
+	define("DB_HOST","67.205.158.181");//ip server
+	define("DB_NAME","zardnet_moochcd");//database
+	define("DB_USERNAME","zardnet_moochcd");//user
+	define("DB_PASSWORD", "Stiven!2021@");//password
 	define("DB_ENCODE","utf8");//encode
 	define("PROJ_NAME","pj_notasmooc");//project name
 ?>
